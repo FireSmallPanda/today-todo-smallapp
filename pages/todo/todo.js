0b4b54f65de7962da62f4484d9651f53348abc2b
weihao_ling <1020529941@qq.com>
2020-03-06 17:37:54 +0800

    feat:初始代码提交

---
+++ pages/todo/todo.js
@@ -0,0 +1,19 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+    //打开一个新页面
+    wx.switchTab({
+      url: '../todo/todo',
+    })
+  }
+})
